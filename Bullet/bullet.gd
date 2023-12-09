efd12fe56950f9cd7eb4efdce67b789f15b8f9fa
David <37672966+Ieida@users.noreply.github.com>
2023-12-09 11:13:42 +0100

    minor changes
    adapting for the new project direction

--- Bullet/bullet.gd
+++ Bullet/bullet.gd
@@ -1,13 +1,7 @@
 extends CharacterBody3D
 
 
-@export_range(0, 1000) var speed = 5.0
-var target: Node3D
-@export_range(0, 100) var turn_speed: float = 1.0
-
-
-func _process(delta):
-	if target: turn_towards_target(delta)
+@export var speed = 5.0
 
 
 func _physics_process(delta):
@@ -15,12 +9,3 @@ func _physics_process(delta):
 	var coll = move_and_collide(mtn)
 	if coll:
 		queue_free()
-
-
-func set_target(new_target: Node3D):
-	target = new_target
-
-
-func turn_towards_target(delta):
-	var to = transform.looking_at(target.position)
-	transform = transform.interpolate_with(to, turn_speed * delta)
--- Player/player.gd
+++ Player/player.gd
@@ -12,7 +12,6 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var is_shooting = false
 @export_range(0, 50000) var rpm: float = 500
 var bullet: PackedScene = preload("res://Bullet/bullet.tscn")
-var targets: Array = []
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -28,19 +27,13 @@ func _input(event):
 
 
 func _process(delta):
-	var inpt_dir = Input.get_axis("look_clockwise", "look_counterclockwise")
-	$Camera3D.rotate($Camera3D.transform.basis.z, inpt_dir * 2 * delta)
-	$Camera3D.orthonormalize()
-	
 	# shooting
 	if Input.is_action_pressed("shoot"):
 		shoot()
-	
-	update_crosshair()
 
 
 func _physics_process(delta):
-	var input_dir = Vector3(Input.get_axis("fly_left", "fly_right"), Input.get_axis("fly_down", "fly_up"), Input.get_axis("fly_forward", "fly_backward")).normalized()
+	var input_dir = Vector3(Input.get_axis("move_left", "move_right"), 0, Input.get_axis("move_forward", "move_backward")).normalized()
 	var direction = $Camera3D.transform.basis * input_dir
 	if direction:
 		velocity = direction * SPEED
@@ -52,35 +45,15 @@ func _physics_process(delta):
 	move_and_slide()
 
 
-func update_crosshair():
-	if targets.size() > 0:
-		$TargetCrosshair/TextureRect.show()
-		$TargetCrosshair.position = targets[0].position
-		$TargetCrosshair/TextureRect.set_position($Camera3D.unproject_position($TargetCrosshair.position))
-	elif $TargetCrosshair.is_visible_in_tree():
-		$TargetCrosshair/TextureRect.hide()
-
-
 func shoot():
-	if is_shooting or not $TargetCrosshair/TextureRect.is_visible_in_tree(): return
+	if is_shooting: return
 	is_shooting = true
 	
 	# shoot
 	var b = bullet.instantiate()
-	b.set_target($TargetCrosshair)
 	b.add_collision_exception_with(self)
-	b.transform = $Camera3D.transform.looking_at($Camera3D.transform.basis.y)
+	b.transform = $Camera3D.transform
 	add_child(b)
 	await get_tree().create_timer(60.0 / rpm).timeout
 	
 	is_shooting = false
-
-
-func _on_target_area_body_entered(body):
-	if body is Enemy:
-		targets.append(body)
-
-
-func _on_target_area_body_exited(body):
-	if targets.has(body):
-		targets.erase(body)

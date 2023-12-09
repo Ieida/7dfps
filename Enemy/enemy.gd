7f98312bfd24b4a64b22e92712f48f053c2e7a82
David <37672966+Ieida@users.noreply.github.com>
2023-12-09 19:28:53 +0100

    enemy, shooting, player

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -2,5 +2,40 @@ extends CharacterBody3D
 class_name Enemy
 
 
+var movement_speed: float = 0.5
+var target: Node3D = null
+
+@onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
+
+
+func _ready():
+	target = get_tree().get_nodes_in_group("players")[0]
+
+	# Make sure to not await during _ready.
+	call_deferred("actor_setup")
+
+
+func actor_setup():
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
+
+	# Now that the navigation map is no longer empty, set the movement target.
+	while true:
+		set_movement_target(target.position)
+		await get_tree().create_timer(0.5).timeout
+
+
+func set_movement_target(movement_target: Vector3):
+	navigation_agent.set_target_position(movement_target)
+
+
 func _physics_process(delta):
-	pass
+	if navigation_agent.is_navigation_finished():
+		return
+
+	var current_agent_position: Vector3 = global_position
+	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
+	print(next_path_position)
+
+	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+	move_and_slide()
--- Player/player.gd
+++ Player/player.gd
@@ -1,17 +1,19 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
+@export var speed = 2.0
 @export var sensitivity: float = 2
 var is_shooting = false
 @export_range(0, 50000) var rpm: float = 500
 var bullet: PackedScene = preload("res://Bullet/bullet.tscn")
+var rot_x = 0.0
+var rot_y = 0.0
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -19,9 +21,11 @@ func _ready():
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		$Camera3D.rotate($Camera3D.transform.basis.y, -event.relative.x * sensitivity)
-		$Camera3D.rotate($Camera3D.transform.basis.x, -event.relative.y * sensitivity)
-		$Camera3D.orthonormalize()
+		rot_x += -event.relative.x * sensitivity
+		rot_y += -event.relative.y * sensitivity
+		$Camera3D.transform.basis = Basis() # reset rotation
+		$Camera3D.rotate_object_local(Vector3(0, 1, 0), rot_x) # first rotate in Y
+		$Camera3D.rotate_object_local(Vector3(1, 0, 0), rot_y) # then rotate in X
 	elif event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED else Input.MOUSE_MODE_CAPTURED
 
@@ -33,14 +37,20 @@ func _process(delta):
 
 
 func _physics_process(delta):
-	var input_dir = Vector3(Input.get_axis("move_left", "move_right"), 0, Input.get_axis("move_forward", "move_backward")).normalized()
-	var direction = $Camera3D.transform.basis * input_dir
-	if direction:
-		velocity = direction * SPEED
+	# gravity
+	if not is_on_floor():
+		velocity += Vector3(0, -1, 0) * gravity * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.y = 0
+	
+	var hor = $Camera3D.global_basis.x * Input.get_axis("move_left", "move_right")
+	var ver = global_basis.y.cross($Camera3D.global_basis.x) * Input.get_axis("move_backward", "move_forward")
+	var direction = (hor.normalized() + ver.normalized()).normalized()
+	if direction:
+		velocity = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
 
